51877c8ec2fbce22aa0d4c92fcf12f5944250e22
ArthurRenosto <arthurrenosto123@gmail.com>
2026-08-30 19:07:50 -0300

    simplificando front navbar

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
     include_once("templates/header.php")
 ?>
-<body>
-
-
     <main>
 
         <div class="create-note">
@@ -12,50 +9,38 @@
 
         </div>
 
+        <article class="note">
 
-        <section class="notes">
+            <div class="note-top">
 
+            <?php foreach($notes as $jorge): ?>
+                <?= $jorge["title"] ?>
+            <?php endforeach; ?>
 
-            <article class="note">
+                <button> <i class="bi bi-pin-angle"></i> </button>
 
-                <div class="note-top">
+            </div>
 
-                <?php foreach($notes as $note): ?>
-                    <?= $note["name"] ?>
-                <?php endforeach; ?>
+            <?php foreach($notes as $note): ?>
+                <?= $note["note"] ?>
+            <?php endforeach; ?>
 
-                    <button> <i class="bi bi-pin-angle"></i> </button>
+            <div class="note-footer">
 
-                </div>
+            <button class="edit-btn">
+                <i class="bi bi-pencil"></i>
+            </button>
+            
+            <button class="delete-btn">
+                <i class="bi bi-trash3"></i>
+            </button>
+            
+            </div>
 
-                <?php foreach($notes as $note): ?>
-                    <?= $note["note"] ?>
-                <?php endforeach; ?>
+        </article>
 
-                <div class="note-footer">
-
-                  <button class="edit-btn">
-                    <i class="bi bi-pencil"></i>
-                </button>
-                
-                <button class="delete-btn">
-                    <i class="bi bi-trash3"></i>
-                </button>
-                
-                </div>
-
-            </article>
-
-
- 
-
-        </section>
 
     </main>
-
-</body>
-
-
 <?php
     include_once("templates/footer.php")
 ?>
--- templates/header.php
+++ templates/header.php
@@ -8,28 +8,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda de contatos</title>
-    <!-- BOOTSTRAP -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.min.css"> 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <!-- FONT AWESOME -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.1/css/all.min.css" integrity="sha512-QeR2VH+lsBE5LSAe1Q5EnTBbe7XTBubt8dG93Y7gidSgdMCr8nVqKcfKAMyN96SV8KDbZVTDXChatu5G2KQGzg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS -->
-     <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Agenda de contatos</title>
+  <!-- BOOTSTRAP -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.min.css"> 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <!-- FONTES -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+  <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+
 </head>
+
 <body>
+
     <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
-        <img src="<?= $BASE_URL ?>img/logo.png" alt="balioAndNavi">
-      </a>
-      <div>
-        <div class="navbar-nav">
-          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Notas</a>
-          <a class="nav-link active" href="<?= $BASE_URL ?>create.php">Imagens</a>
+    
+      <nav class="navbar navbar-expand-lg">
+    
+        <div class="navbar-nav mx-auto">
+    
+          <a class="nav-link" href="<?= $BASE_URL ?>index.php">Notas</a>
+        
+          <a class="navbar-brand mx-3" href="<?= $BASE_URL ?>index.php">
+          <img src="<?= $BASE_URL ?>img/home_img.png" alt="balioAndNavi"> <!-- Imagem -->
+          </a>
+
+          <a class="nav-link" href="<?= $BASE_URL ?>galery.php">Galeria</a>
+    
         </div>
-      </div>
-    </nav>
-    </header>
+
+      </nav>
+    
+   </header>
